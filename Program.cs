c85dcbb344b92ba3293e79632b458a463ad62bda
FIC <lqa.gabriel@gmail.com>
2017-10-06 19:29:10 -0300

    Feito e testado, de maneira básica direto no main

--- Program.cs
+++ Program.cs
@@ -16,16 +16,45 @@ namespace aula0610
             string rg;
             string dataNascimento;
             string sexo;
-            string estdoCivil;
+            string estadoCivil;
             string profissao;
 
+            Console.WriteLine("DADOS CADASTRAIS\nDigite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu endereço");
+            endereco = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome da sua cidade onde você mora");
+            cidade = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do estado onde você mora");
+            estado = Console.ReadLine();
+
+            Console.WriteLine("Digite o cep da rua onde você mora");
+            cep = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu CPF");
+            cpf = Console.ReadLine();
             
+            Console.WriteLine("Digite o seu RG");
+            rg = Console.ReadLine();
 
+            Console.WriteLine("Digite a data do seu nascimento");
+            dataNascimento = Console.ReadLine();
 
-            Console.WriteLine("DADOS CADASTRAIS");
+            Console.WriteLine("Digite seu sexo");
+            sexo = Console.ReadLine();
 
-            Console.Write("NOME: ");
-            Console.ReadLine
+            Console.WriteLine("Digite o seu estado civil");
+            estadoCivil = Console.ReadLine();
+
+            Console.WriteLine("Digite sua profissão");
+            profissao = Console.ReadLine();
+
+            Console.WriteLine("Você se chama {0}, mora em {1}, cidade {2}, estado {3}, cep {4}, seu cpf é {5}, seu rg é {6}, nasceu em {7}, sexo {8}, está {9}, e é {10}",nome, endereco, cidade, estado, cep, cpf, rg, dataNascimento, sexo, estadoCivil, profissao);
+            
+            Console.Read();            
         }
     }
 }
