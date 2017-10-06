f33762e0616cd64f8a3c861237ac3e82c55183e5
FIC <lqa.gabriel@gmail.com>
2017-10-06 20:20:38 -0300

    Agora com array

--- Program.cs
+++ Program.cs
@@ -5,56 +5,29 @@ namespace aula0610
     class Program
     {
         static void Main(string[] args)
-        {
-            
-            string nome;
-            string endereco;
-            string cidade;
-            string estado;
-            string cep;
-            string cpf;
-            string rg;
-            string dataNascimento;
-            string sexo;
-            string estadoCivil;
-            string profissao;
+        {      
+            string[,] dados = new string[11,2]{{"nome",""},
+                                               {"endereco",""},
+                                               {"cidade",""},
+                                               {"estado",""},
+                                               {"cep",""},
+                                               {"cpf",""},
+                                               {"rg",""},
+                                               {"data de nascimento",""},
+                                               {"sexo",""},
+                                               {"estado civíl",""},
+                                               {"profissão",""}};
+                                               
+            for (int i = 0; i <= dados.GetUpperBound(0); i++)
+            {
+                Console.WriteLine("Digite a seguinte informação: " + dados[i,0]); 
+                dados[i,1] = Console.ReadLine();
+            }
 
-            Console.WriteLine("DADOS CADASTRAIS\nDigite seu nome");
-            nome = Console.ReadLine();
+            Console.WriteLine("Você se chama {0}, mora em {1}, cidade {2}, estado {3}, cep {4}, seu cpf é {5}, seu rg é {6}, nasceu em {7}, sexo {8}, está {9}, e é {10}", dados[0,1], dados[1,1], dados[2,1], dados[3,1], dados[4,1], dados[5,1], dados[6,1], dados[7,1],dados[8,1], dados[9,1], dados[10,1] );
+            Console.Read();     
 
-            Console.WriteLine("Digite seu endereço");
-            endereco = Console.ReadLine();
 
-            Console.WriteLine("Digite o nome da sua cidade onde você mora");
-            cidade = Console.ReadLine();
-
-            Console.WriteLine("Digite o nome do estado onde você mora");
-            estado = Console.ReadLine();
-
-            Console.WriteLine("Digite o cep da rua onde você mora");
-            cep = Console.ReadLine();
-
-            Console.WriteLine("Digite o seu CPF");
-            cpf = Console.ReadLine();
-            
-            Console.WriteLine("Digite o seu RG");
-            rg = Console.ReadLine();
-
-            Console.WriteLine("Digite a data do seu nascimento");
-            dataNascimento = Console.ReadLine();
-
-            Console.WriteLine("Digite seu sexo");
-            sexo = Console.ReadLine();
-
-            Console.WriteLine("Digite o seu estado civil");
-            estadoCivil = Console.ReadLine();
-
-            Console.WriteLine("Digite sua profissão");
-            profissao = Console.ReadLine();
-
-            Console.WriteLine("Você se chama {0}, mora em {1}, cidade {2}, estado {3}, cep {4}, seu cpf é {5}, seu rg é {6}, nasceu em {7}, sexo {8}, está {9}, e é {10}",nome, endereco, cidade, estado, cep, cpf, rg, dataNascimento, sexo, estadoCivil, profissao);
-            
-            Console.Read();            
         }
     }
 }
