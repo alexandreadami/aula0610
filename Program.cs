f0acc1e59c502328c4663650a0bcb92245207658
FIC <alexandre.adami@uol.com.br>
2017-10-06 18:50:55 -0300

    comentario

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace aula0610
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nome;
+            string endereco;
+            string cidade;
+            string estado;
+            string cep;
+            string cpf;
+            string rg;
+            string dataNascimento;
+            string sexo;
+            string estdoCivil;
+            string profissao;
+
+            
+
+
+            Console.WriteLine("DADOS CADASTRAIS");
+
+            Console.Write("NOME: ");
+            Console.ReadLine
+        }
+    }
+}
